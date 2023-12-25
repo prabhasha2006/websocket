7e6926913ad8e9b1cb40436da032aa04307f2bab
Achira Gaming <86994940+achiragaming@users.noreply.github.com>
2023-12-25 23:50:08 +0530

    update

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const workoutRoutes = require('./routes/workouts')
-const socketio = require('socket.io')(5556, {
-    cors: {
-        origin: ['http://localhost:5557/']
-    }
-})
-//app
-
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const workoutRoutes = require("./routes/workouts");
+const socketIo = require("socket.io");
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(`${process.cwd()}/website`));
+app.use(bodyParser.json({ limit: "1000mb" }));
+app.set("trust proxy", true);
 app.use((request, res, next) => {
-    console.log(request.path, request.method)
-    next()
-})
-app.use('/api/workouts', workoutRoutes)
+  console.log(request.path, request.method);
+  next();
+});
+app.use("/api/workouts", workoutRoutes);
 //router
-app.get('/', (request, res) => {
-    //res.json({mssg: "Hello Kumuthu"})
-    res.sendFile('./website/index.html', { root: __dirname })
-}) 
+app.get("/", (request, res) => {
+  //res.json({mssg: "Hello Kumuthu"})
+  res.sendFile("./website/index.html", { root: __dirname });
+});
 //listen
-app.listen(process.env.PORT, () => {
-    console.log('Listerning Port => ',process.env.PORT)
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log("Listerning Port => ", process.env.PORT);
+});
 
 //websocker
-socketio.on('connection', socket => {
-    console.log('connected..! ', socket.id)
-})
+
+const io = socketIo(server, {
+  cors: {
+    origin: process.env.FRONTEND_URI,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  },
+  transports: ["polling", "websocket"], // Enable WebSocket transport
+});
+io.on("connection", (socket) => {
+  console.log("connected");
+});
