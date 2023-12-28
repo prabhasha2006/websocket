38e5699f79ddc924c1694b8b31fc9b935fe607e8
EveloCore <102839813+prabhasha2006@users.noreply.github.com>
2023-12-28 20:33:27 +0530

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -1,46 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const workoutRoutes = require('./routes/workouts')
-//app
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const workoutRoutes = require("./routes/workouts");
+const socketIo = require("socket.io");
 
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(express.static(`${process.cwd()}/website`));
+app.use(bodyParser.json({ limit: "1000mb" }));
+app.set("trust proxy", true);
 app.use((request, res, next) => {
-    console.log(request.path, request.method)
-    next()
-})
-app.use('/api/workouts', workoutRoutes)
+  console.log(request.path, request.method);
+  next();
+});
+app.use("/api/workouts", workoutRoutes);
 //router
-app.get('/', (request, res) => {
-    res.json({mssg: "Hello Kumuthu"})
-    res.sendFile('/index.html', { root: __dirname })
-}) 
+app.get("/", (request, res) => {
+  //res.json({mssg: "Hello Kumuthu"})
+  res.sendFile("./website/index.html", { root: __dirname });
+});
 //listen
-app.listen(process.env.PORT, () => {
-    console.log('Listerning Port => ',process.env.PORT)
-})
+const server = app.listen(process.env.PORT, () => {
+  console.log("Listerning Port ==> ", process.env.PORT);
+});
 
 //websocker
-/*const express = require('express')
-const webserver = express()
- .use((req, res) =>
-   res.sendFile('/index.html', { root: __dirname })
- )
- .listen(2006, () => console.log(`RedDragon eveocode hosting on ${2006}`))
-const { WebSocketServer } = require('ws')
-const sockserver = new WebSocketServer({ port: 443 })
-sockserver.on('connection', ws => {
- console.log('New client connected!')
- ws.send('connection established')
- ws.on('close', () => console.log('Client has disconnected!'))
- ws.on('message', data => {
-   sockserver.clients.forEach(client => {
-     console.log(`distributing message: ${data}`)
-     client.send(`${data}`)
-   })
- })
- ws.onerror = function () {
-   console.log('websocket error')
- }
-})*/
+const io = socketIo(server, {
+  cors: {
+    origin: process.env.FRONTEND_URI,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  },
+  transports: ["polling", "websocket"], // Enable WebSocket transport
+});
+io.on("connection", (socket) => {
+  console.log("connected");
+});
